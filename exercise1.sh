32b403b15d4fe6eb79165f01b2a3378f19c63e9b
kkim723 <81382132+kkim723@users.noreply.github.com>
2021-03-25 13:47:46 -0400

    Add files via upload

---
+++ exercise1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo 'Hello world'
+echo 'Hello world'
+echo 'Hello world'
+echo 'Hello world'
+echo 'Hello world'
+echo 'Hello world'
+echo 'Hello world'
+echo 'Hello world'
+echo 'Hello world'
+echo 'Hello world'
+
+sleep 2s
+
+ls -a | wc -l > exercise1.txt
+
+
